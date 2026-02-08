38874129770fe071e07defe9b07b83fd0b1341cf
K Sudarshan Hebbar <162970218+KSudarshanH@users.noreply.github.com>
2026-02-08 18:23:12 +0530

    Staff and admin colors and animation corrected

---
+++ pos-resto-merged/app/staff/layout.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import "../globals.css"
+import { usePathname } from 'next/navigation';
+
+import { AuthProvider } from "./contexts/AuthContext"
+import { OrdersProvider } from "./contexts/OrdersContext"
+import { NavigationProvider } from "./contexts/NavigationContext"
+
+
+import { StaffNavigation } from "./components/StaffNavigation"
+
+export default function StaffLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const pathname = usePathname();
+  const isLoginPage = pathname === '/staff/staff-login';
+
+  return (
+    <div className="min-h-screen bg-linear-to-br from-ivory via-white to-ivory">
+      <AuthProvider>
+        <OrdersProvider>
+          <NavigationProvider>
+            {!isLoginPage && <StaffNavigation />}
+            <main className={`${!isLoginPage ? 'lg:pl-64 pb-20 lg:pb-0' : ''} min-h-screen`}>
+                {children}
+            </main>
+          </NavigationProvider>
+        </OrdersProvider>
+      </AuthProvider>
+    </div>
+  )
+}
+
+
